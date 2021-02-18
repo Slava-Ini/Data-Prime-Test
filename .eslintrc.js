b1379cee2c4b8afa471193d3f8045dd291940f99
Slava-Ini <vyacheslav.inyutochkin@gmail.com>
2021-02-18 17:55:39 +0900

    Leftover files

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'react/react-in-jsx-scope': 'off',
+		'linebreak-style': 0,
+		'global-require': 0,
+		'eslint linebreak-style': [0, 'error', 'windows']
+	}
+};
